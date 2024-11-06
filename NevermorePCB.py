c5e1509d4bc29063b2142c31dd1986d18c26e86e
Nicolas Slammy Outrey <nicolas.outrey@gmail.com>
2024-11-06 15:48:02 +0100

    Initialize Repo

---
+++ NevermorePCB.py
@@ -0,0 +1,11 @@
+class NevermoreSensor:
+    def __init__(self,config):
+    
+    
+    def handle_connect(self):
+        
+    def setup_minmax(self,min_temp,max_temp)
+    def setup_callback()
+    def ger_report_time_delta()
+    def get_status()
+    
